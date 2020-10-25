8e1b604aa033894b759ad24712e98a87196a6802
MaciekN <me@maciekn.pl>
2020-10-25 16:09:23 +0100

    send data to thingspeak

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Description
-This simple app is intended to read from solar driver via Serial protocol and then push it to MQQT
+This simple app is intended to read from solar driver via Serial protocol and then push it to ThingSpeak
 The app also supports debug logging via Hardware Serial protocol.
 
 !!!Currently is heavily WIP!!!
 
 # Configuration
 
+
+## Hardware configuration
 The most important part is to select appropriate pins for communication with the device:
 ```
 int tx_pin = D1;
@@ -20,3 +22,7 @@ It is also important to bind right definitions of serials to the reader:
                                 |           |
 CoReader coReader = CoReader(InputSerial, Serial);
 ```
+
+## Software configuration
+WiFi settings and ThingSpeak configuration (Channel ID and API key) need to be updated upon configuration.
+After first run app creates `AutoConnectAP` WiFi network. After connecting by some mobile phone it should redirect to configuration screen.
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
+#include <ArduinoJson.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <LittleFS.h>
 #include <SoftwareSerial.h>
+#include <ThingSpeak.h>
+#include <WiFiManager.h>
 
 #include "coreader.h"
 
@@ -11,16 +18,110 @@ const ushort water_temp_id = 0x1702;      // alternative: 0x1743?
 const ushort timestamp_id = 0x1620;
 const ushort sth_else_id = 0x1B98;
 
+const char CONFIG_LOCATION[] = "/config.json";
+
+char myChannelNumber[20];
+char myWriteAPIKey[34];
+
+WiFiClient espClient;
+ESP8266WebServer server(80);
+
+bool shouldSaveConfig;
+
+void saveConfigCallback() {
+    shouldSaveConfig = true;
+}
+
+void loadStoredData() {
+    Serial.println("Opening file");
+    if (LittleFS.exists(CONFIG_LOCATION)) {
+
+
+        File configFile = LittleFS.open(CONFIG_LOCATION, "r");
+        Serial.println("File opened");
+        if (configFile) {
+            DynamicJsonDocument jsonBuffer(1024);
+            Serial.println("Content loaded");
+            if (deserializeJson(jsonBuffer, configFile) ==
+                DeserializationError::Ok) {
+                JsonObject json = jsonBuffer.as<JsonObject>();
+                strcpy(myChannelNumber, json["channel"]);
+                strcpy(myWriteAPIKey, json["apiKey"]);
+            } else {
+                Serial.println("failed to load json config");
+            }
+            configFile.close();
+        }
+    }
+}
+
+void initalizeWiFiManager() {
+    WiFiManagerParameter custom_channel_no("channel", "MQQT Channel",
+                                           myChannelNumber, 20);
+    WiFiManagerParameter custom_api_key("apiKey", "API Key", myWriteAPIKey, 32);
+
+    WiFiManager wifiManager;
+
+    wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+    wifiManager.addParameter(&custom_channel_no);
+    wifiManager.addParameter(&custom_api_key);
+
+    wifiManager.autoConnect("AutoConnectAP");
+    
+    delay(500);
+
+    strcpy(myChannelNumber, custom_channel_no.getValue());
+    strcpy(myWriteAPIKey, custom_api_key.getValue());
+}
+
+void cleanData() {
+    Serial.println("Clearing settings!");
+    LittleFS.remove(CONFIG_LOCATION);
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+}
+
 void setup() {
     pinMode(rx_pin, INPUT_PULLUP);
     pinMode(tx_pin, INPUT_PULLUP);
+    pinMode(0, INPUT_PULLUP);
+
     Serial.begin(9600);
-    while (!Serial)
-        ;
+    while (!Serial);
     InputSerial.begin(9600);
-    while (!InputSerial)
-        ;
-    delay(500);
+    while (!InputSerial);
+
+    LittleFS.begin();
+
+    Serial.println("Welcome to the MQQT jungle!");
+    Serial.println("Press flash button now to reset settings");
+    delay(2000);
+    if (digitalRead(0) == 0) {
+        cleanData();
+    }
+
+    loadStoredData();
+
+    initalizeWiFiManager();
+
+    if (shouldSaveConfig) {
+        Serial.println("saving config");
+        DynamicJsonDocument jsonBuffer(1024);
+        JsonObject json = jsonBuffer.to<JsonObject>();
+        json["channel"] = myChannelNumber;
+        json["apiKey"] = myWriteAPIKey;
+
+        File configFile = LittleFS.open(CONFIG_LOCATION, "w");
+        if (!configFile) {
+            Serial.println("failed to open config file for writing");
+        }
+        serializeJson(jsonBuffer, configFile);
+        configFile.close();
+    }
+
+    ThingSpeak.begin(espClient);
+
 }
 
 struct Payload {
@@ -30,22 +131,26 @@ struct Payload {
     ushort min = 0;
 };
 
-void decodeInput(ushort* buffer, int len, Payload* dest) {
+int decodeInput(ushort* buffer, int len, Payload* dest) {
+    int noOfValues = 0;
     for (int i = 0; i < (len - 1); i += 2) {
         switch (buffer[i]) {
             case collector_temp_id:
                 dest->collector_temp = buffer[i + 1];
+                noOfValues++;
                 break;
             case water_temp_id:
                 dest->water_temp = buffer[i + 1];
+                noOfValues++;
                 break;
             case timestamp_id:
                 dest->hour = (buffer[i + 1] & 0xFF00) >> 8;
                 dest->min = buffer[i + 1] & 0xFF;
+                noOfValues++;
                 break;
         }
     }
-
+    return noOfValues;
 }
 
 ushort buffer[300];
@@ -57,15 +162,18 @@ void loop() {
 
     if (len > 0) {
         Payload data;
-        decodeInput(buffer, len, &data);
-        Serial.println("fetched data:");
-        Serial.print("Watertemp: ");
-        Serial.println(data.water_temp);
-        Serial.print("Collectortemp: ");
-        Serial.println(data.collector_temp);
-        Serial.print("Timestamp: ");
-        Serial.print(data.hour);
-        Serial.print(":");
-        Serial.println(data.min);
+        if (decodeInput(buffer, len, &data)) {
+            Serial.println("Sending data to ThingSpeak");
+            ThingSpeak.setField(1, data.water_temp);
+            ThingSpeak.setField(2, data.collector_temp);
+            int httpCode =
+                ThingSpeak.writeFields(atol(myChannelNumber), myWriteAPIKey);
+            if (httpCode == 200) {
+                Serial.println("Channel write successful.");
+            } else {
+                Serial.println("Problem writing to channel. HTTP error code " +
+                               String(httpCode));
+            }
+        }
     }
 }
